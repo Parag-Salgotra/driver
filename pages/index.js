12f1ae2d993827a8692558c1ad770831dbe26fa6
parag <parag.salgotra.1000@gmail.com>
2024-04-10 05:00:25 -0300

    Action Buttons Fixed

--- pages/index.js
+++ pages/index.js
@@ -47,29 +47,26 @@ export default function Home() {
         </Header>
 
         <ActionButtons>
-        
-            <ActionButton>
             <Link href="/search">
+            <ActionButton>
                 <ActionButtonImage src="./Icons/Ride.png"/> 
-            </Link>
-            Ride
+                Ride
             </ActionButton>
+            </Link>
           
-
-            <ActionButton>
             <Link href="/search">
+            <ActionButton>
               <ActionButtonImage src="./Icons/Wheels.png"/>
-            </Link>
-            Wheels
+              Wheels
             </ActionButton>
+            </Link>
 
-       
-            <ActionButton>
             <Link href="/search">
+            <ActionButton>
               <ActionButtonImage src="./Icons/Reserve.png"/>
-            </Link>
-            Reserve
+              Reserve
             </ActionButton>
+            </Link>
 
         </ActionButtons>
 
@@ -82,14 +79,13 @@ export default function Home() {
   );
 }
 const Wrapper = tw.div`
-flex flex-col
 h-screen
 bg-white
 text-black
 `
 const ActionItems = tw.div`
 p-4 absolute bottom-10 right-10 left-10 z-10
-bg-white rounded-lg
+bg-white rounded-lg 
 `
 const Header = tw.div`
 flex justify-between items-center
@@ -107,17 +103,17 @@ const UserImage = tw.img`
 h-12 w-12 rounded-full border-gray-200 p-px cursor-pointer
 `
 const ActionButtons = tw.div`
-flex
+grid grid-cols-3
 `
 const ActionButton = tw.div`
 flex 
-bg-gray-200 flex-1 m-1 h-32 
+bg-gray-200 m-1 h-32 
 items-center flex-col justify-center
 rounded-lg text-xl
 transform hover:scale-95 transition
 `
 const ActionButtonImage = tw.img`
-h-24
+h-24 
 `
 const InputButtons = tw.div`
 h-20 bg-gray-200 text-2xl p-4 mt-8 
